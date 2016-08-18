4c18cc691869a8e10ef60c9c84ee2e11b577faa5
zhengshaopengCQ <407987819@qq.com>
2016-08-18 17:53:40 +0800

    no message

--- src/com/example/photo/GerenFragment.java
+++ src/com/example/photo/GerenFragment.java
@@ -22,25 +22,21 @@ public class GerenFragment extends Fragment {
 	ImageView imageview;
 	ImageView imageview2;
 	SlidingMenu mSlidingMenu;
-TextView geduo;
+	TextView geduo;
+
 	public View onCreateView(LayoutInflater inflater, ViewGroup container,
 			Bundle savedInstanceState) {
-<<<<<<< HEAD
 		View v = inflater.inflate(R.layout.activity_geren, container, false);
 		listView = (ListView) v.findViewById(R.id.listview_geren);
 		mSlidingMenu = (SlidingMenu) getActivity().findViewById(R.id.id_menu);
-=======
-		View v = inflater.inflate(R.layout.photo_main, container, false);
-		listView = (ListView) v.findViewById(R.id.listView_dowm);
->>>>>>> master
 
 		View mainView = inflater.inflate(R.layout.activity_gerenzhongxin, null,
 				false);
 		mSlidingMenu = (SlidingMenu) mainView.findViewById(R.id.id_menu);
-		
-		imageview2=(ImageView)v.findViewById(R.id.photomain_imageview2);
+
+		imageview2 = (ImageView) v.findViewById(R.id.photomain_imageview2);
 		imageview2.setOnClickListener(onClick);
-		
+
 		imageview = (ImageView) v.findViewById(R.id.photomain_imageview);
 		imageview.setOnClickListener(onClick);
 
@@ -51,72 +47,65 @@ TextView geduo;
 	OnClickListener onClick = new OnClickListener() {
 		public void onClick(View v) {
 			switch (v.getId()) {
-<<<<<<< HEAD
-				case R.id.image_geren:
-					mSlidingMenu.toggle();
-					break;
-				case R.id.ziliao_image:
-					Intent intent=new Intent(getActivity(),ZiliaoActivity.class);
-					startActivity(intent);
-					break;
-				default:
-					break;
-=======
+
+			case R.id.image_geren:
+				mSlidingMenu.toggle();
+				break;
+			case R.id.ziliao_image:
+				Intent intent = new Intent(getActivity(), ZiliaoActivity.class);
+				startActivity(intent);
+				break;
+			default:
+				break;
+
 			case R.id.photomain_imageview:
 				mSlidingMenu.openMenu();
 				break;
 			case R.id.photomain_imageview2:
-				Intent intent=new Intent(getActivity(),ZiliaoActivity.class);
+				intent = new Intent(getActivity(), ZiliaoActivity.class);
 				startActivity(intent);
 				break;
->>>>>>> master
+
 			}
 		}
 	};
 
 	public void getDataDowm() {
-<<<<<<< HEAD
-=======
 
-		
->>>>>>> master
 		LayoutInflater inflater = getLayoutInflater(getArguments());
 		View headView = inflater.inflate(R.layout.photo_main_head, null);
 		listView.addHeaderView(headView);
-		geduo=(TextView)headView.findViewById(R.id.geduo);
-		
-		
+		geduo = (TextView) headView.findViewById(R.id.geduo);
+
 		LayoutInflater inflater1 = getLayoutInflater(getArguments());
-		View view2= inflater1.inflate(R.layout.activity_gerenzhiliao, null);
-		TextView DengDaiGeGuo=(TextView)view2.findViewById(R.id.waitingGeduo);
-		String GeDuo=(String) DengDaiGeGuo.getText();
-		Log.i("GeDuo",""+GeDuo);
-		
-		geduo.setText(GeDuo);
+		View view2 = inflater1.inflate(R.layout.activity_gerenzhiliao, null);
+		//TextView DengDaiGeGuo = (TextView) view2
+			//	.findViewById(R.id.waitingGeduo);
+		//String GeDuo = (String) DengDaiGeGuo.getText();
+	//Log.i("GeDuo", "" + GeDuo);
+
+	//	geduo.setText(GeDuo);
 		for (int i = 0; i < 3; i++) {
 			Photo photoinfo = new Photo();
 
-<<<<<<< HEAD
-		PhotoInfo photoinfo = new PhotoInfo();
-		photoinfo.setImageBackground(R.drawable.xiaoyuan);
-		photoinfo.setImageHead(R.drawable.head);
-		photoinfo.setName("Leo");
-		photoinfo.setTime("2天前");
-		photoinfo.setText("那年夏天，我们\n最后" + " 一次走过校园的林荫大道， "
-				+ " \n走向野心勃勃欲不可知的未来");
-		photoinfo.setLastCard(R.drawable.lastactivity);
-		list.add(photoinfo);
-=======
+			//photoinfo.setImageBackground(R.drawable.xiaoyuan);
 			photoinfo.setImageHead(R.drawable.head);
->>>>>>> master
+			photoinfo.setName("Leo");
+			photoinfo.setTime("2天前");
+			photoinfo.setText("那年夏天，我们\n最后" + " 一次走过校园的林荫大道， "
+					+ " \n走向野心勃勃欲不可知的未来");
+			photoinfo.setLastCard(R.drawable.lastactivity);
+			list.add(photoinfo);
+
+			photoinfo.setImageHead(R.drawable.head);
 
 			list.add(photoinfo);
 		}
 		PhotoAdapter myAdapter = new PhotoAdapter(R.layout.photo_main_down,
 				list, getActivity());
 		listView.setAdapter(myAdapter);
-		//通过list长度设置卡片张数
-		Object[] numb= list.toArray();// 返回列表所有元素
+		// 通过list长度设置卡片张数
+		Object[] numb = list.toArray();// 返回列表所有元素
 		int num = numb.length;
 		String str = String.valueOf(num);
 		TextView textView = (TextView) headView.findViewById(R.id.textview_one);
@@ -127,7 +116,5 @@ TextView geduo;
 		// TODO Auto-generated method stub
 		return null;
 	}
-	
-	
 
 }
