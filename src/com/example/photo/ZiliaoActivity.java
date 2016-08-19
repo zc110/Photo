c4f7a0a8ff6d04647d30e54dcc02619edf94b587
zhaoliusen999 <821448155@qq.com>
2016-08-19 08:23:13 +0800

    个人资料的修改

--- src/com/example/photo/ZiliaoActivity.java
+++ src/com/example/photo/ZiliaoActivity.java
@@ -12,74 +12,78 @@ import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemSelectedListener;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
+import android.widget.RelativeLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 
-public class ZiliaoActivity extends Activity{
+public class ZiliaoActivity extends Activity {
 	private static final int RESULT_LOAD_IMAGE = 0;
-	ImageView view_fanhui,view_touxiang,tiaozhuan;
-	TextView view_qianming;
-	Spinner spinner;	
+	protected static final int REQUEST_CODE = 1002;
+	protected static final String intent = null;
+	ImageView view_fanhui, view_touxiang;
+	RelativeLayout tiaozhuan;
+	TextView text_gexing;
+	Spinner spinner;
 	ArrayAdapter<String> adapter;
-	List<String>  list=new ArrayList<String>();
+	List<String> list = new ArrayList<String>();
+
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_gerenzhiliao);
-		view_fanhui=(ImageView)findViewById(R.id.fanhui_geren);
+		view_fanhui = (ImageView) findViewById(R.id.fanhui_geren);
 		view_fanhui.setOnClickListener(l);
-		
-		view_touxiang=(ImageView)findViewById(R.id.touxiang_imageview);
+
+		view_touxiang = (ImageView) findViewById(R.id.touxiang_imageview);
 		view_touxiang.setOnClickListener(l);
-		
-		tiaozhuan=(ImageView)findViewById(R.id.tiaozhuan);
-		tiaozhuan.setOnClickListener(l);
-		
-		view_qianming=(TextView)findViewById(R.id.qianming_text);
-		view_qianming.setOnClickListener(l);
+
+		tiaozhuan = (RelativeLayout) findViewById(R.id.tiaozhuan);
+		tiaozhuan.setOnClickListener(new OnClickListener() {
+			public void onClick(View v) {
+				Intent intent = new Intent(ZiliaoActivity.this,
+						QianmingActivity.class);
+				startActivityForResult(intent, REQUEST_CODE);
+	
+			}
+		});
+		text_gexing=(TextView)findViewById(R.id.text_gexing);
 		String str=getIntent().getStringExtra("qianming");
-		view_qianming.setText(str);
-		spinner=(Spinner)findViewById(R.id.spinner_ziliao);
-		adapter=new ArrayAdapter<String>(ZiliaoActivity.this,
+		text_gexing.setText(str);
+		
+		spinner = (Spinner) findViewById(R.id.spinner_ziliao);
+		adapter = new ArrayAdapter<String>(ZiliaoActivity.this,
 				android.R.layout.simple_spinner_item, list);
 		adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 		list.add("男");
 		list.add("女");
-		spinner.setAdapter(adapter);	
-		spinner.setOnItemSelectedListener(itemSelectedListener);
+		spinner.setAdapter(adapter);
+		spinner.setOnItemSelectedListener(new OnItemSelectedListener() {
+			public void onItemSelected(AdapterView<?> parent, View view,
+					int position, long id) {		
+			}
+			public void onNothingSelected(AdapterView<?> parent) {			
+			}
+		});
 	}
-	OnClickListener l=new OnClickListener() {
+
+	protected void OnActivityResult(int i, int j, Intent intent2) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	OnClickListener l = new OnClickListener() {
 		public void onClick(View v) {
-			switch (v.getId()){
-				case R.id.fanhui_geren:
-					finish();
-					break;
-				case R.id.touxiang_imageview:
-					getContent();
-					break;
-				case R.id.tiaozhuan:
-					Intent intent=new Intent(ZiliaoActivity.this,QianmingActivity.class);
-					startActivity(intent);
-					break;
-				default:
-					break;
+			switch (v.getId()) {
+			case R.id.fanhui_geren:
+				finish();
+				break;
+			case R.id.touxiang_imageview:
+				Intent intent = new Intent(Intent.ACTION_PICK,
+						android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+				startActivityForResult(intent, RESULT_LOAD_IMAGE);
+				break;
+			default:
+				break;
 			}
 		}
 	};
-	OnItemSelectedListener itemSelectedListener=new OnItemSelectedListener(){
-		public void onItemSelected(AdapterView<?> parent, View view,
-				int position, long id) {
-			
-		}
-		public void onNothingSelected(AdapterView<?> parent) {
-			
-		}
-	};
-	public void getContent(){
-		Intent intent=new Intent(Intent.ACTION_PICK, 
-				android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
-		/*intent.setAction(Intent.ACTION_GET_CONTENT);
-		intent.setType("vnd.android.cursor.item/photo");
-		startActivity(intent);*/
-		startActivityForResult(intent, RESULT_LOAD_IMAGE);
-	}
 }
